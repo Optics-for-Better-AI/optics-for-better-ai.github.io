6134a2eec834779a819cbc09e764efc866432686
HaiyangJiang <450199156@qq.com>
2023-04-03 21:59:38 +0900

    first draft version

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop - 52;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
